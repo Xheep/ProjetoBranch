e33d5e1891c01612d0abbc17c2beeae3228ab231
Xheep <147657690+Xheep@users.noreply.github.com>
2023-10-23 16:01:25 -0300

    Update Bora.py

--- Bora.py
+++ Bora.py
@@ -4,6 +4,7 @@ for recebe in range(2):
     nome = input('nome: ')
     idade = int(input('idade: '))
     profissao = (input('profissao: '))
+    genero = input('genêro: )
     lista.append(nome)
     lista.append(idade)
     lista.append(profissao)
