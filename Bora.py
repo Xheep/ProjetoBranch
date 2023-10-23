93a975bf834107a05dd122a058d13a8c3f10abd1
Xheep <147657690+Xheep@users.noreply.github.com>
2023-10-23 15:50:12 -0300

    Create Bora.py
    Simplesmente infinitamente mais facil, cetádoido

---
+++ Bora.py
@@ -0,0 +1,11 @@
+lista = []
+
+for recebe in range(2):
+    nome = input('nome: ')
+    idade = int(input('idade: '))
+    profissao = (input('profissao: '))
+    lista.append(nome)
+    lista.append(idade)
+    lista.append(profissao)
+
+print(lista)
